e954c4b6fba9604a7a5422cb7b5ed7cc65859f88
RoyWallace <etonglixianji@gmail.com>
2016-03-19 16:03:25 +0800

    add fragments

--- app/src/main/java/etong/bottomnavigation/demo/BooksFragment.java
+++ app/src/main/java/etong/bottomnavigation/demo/BooksFragment.java
@@ -2,12 +2,16 @@ package etong.bottomnavigation.demo;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
 public class BooksFragment extends Fragment {
+
+    private RecyclerView recycleView;
 
 
     public BooksFragment() {
@@ -23,6 +27,35 @@ public class BooksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_books, container, false);
+        View contentView = inflater.inflate(R.layout.fragment_books, container, false);
+        recycleView = (RecyclerView) contentView.findViewById(R.id.recycleView);
+        recycleView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        recycleView.setAdapter(new BookAdapter());
+        return contentView;
+    }
+
+    private class BookAdapter extends RecyclerView.Adapter<ViewHolder>{
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+    private class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
---
+++ app/src/main/java/etong/bottomnavigation/demo/NewsstandFragment.java
@@ -0,0 +1,9 @@
+package etong.bottomnavigation.demo;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by hwt on 2016/3/19.
+ */
+public class NewsstandFragment extends Fragment {
+}
